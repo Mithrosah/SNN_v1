6cf49dc93d7017b329daf66161ad0b8994ba5255
Mithrosah <xcr_21@163.com>
2025-08-11 11:35:35 +0800

    Initial commit

--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn.functional as F
 from itertools import combinations
 
+# 通用MAJ，任意奇数个输入比特流
 def MAJ(x: torch.Tensor) -> torch.Tensor:
     """
     Majority-gate for an odd number n of packed bit-streams with batch support.
@@ -35,7 +36,7 @@ def MAJ(x: torch.Tensor) -> torch.Tensor:
 
     return packed
 
-
+# 通用stackMAJ，使用了上面的通用MAJ
 def stackMAJ(x: torch.Tensor, maj_dim: int, strict: bool = True) -> torch.Tensor:
     """
     Stacked majority-gate with batch support using tensorized operations.
@@ -96,7 +97,7 @@ def stackMAJ(x: torch.Tensor, maj_dim: int, strict: bool = True) -> torch.Tensor
     # Return the final single bit-stream
     return current.squeeze(1)  # (batch_size, num_ints)
 
-
+# 通用模拟MAJ，任意奇数个输入
 def MAJ_sim(x: torch.Tensor) -> torch.Tensor:
     '''
     vectorized MAJ, support batch processing
@@ -130,6 +131,7 @@ def MAJ_sim(x: torch.Tensor) -> torch.Tensor:
 
     return result / (2 ** (n - 1)) - 1
 
+# 通用模拟stackMAJ，使用了上面的通用模拟MAJ
 def stackMAJ_sim(x: torch.Tensor, maj_dim: int) -> torch.Tensor:
     '''
     vectorized stackMAJ, support batch processing
@@ -161,7 +163,7 @@ def stackMAJ_sim(x: torch.Tensor, maj_dim: int) -> torch.Tensor:
 
     return x
 
-
+# 对比特流形式的图像数据[N, C, H, W, num_ints]做F.unfold
 def unfold_5d(stream, kernel_size, stride, padding):
     """
     F.unfold() can only process 4D float tensors
@@ -202,10 +204,38 @@ def unfold_5d(stream, kernel_size, stride, padding):
 
     return stream
 
-
+# 三输入MAJ，输入参数是三个比特流堆叠形成的
 def maj3_packed(inputs: torch.Tensor) -> torch.Tensor:
     assert inputs.shape[1] == 3, "Second dimension must be 3"
 
     a, b, c = inputs[:, 0], inputs[:, 1], inputs[:, 2]
     return (a & b) | (a & c) | (b & c)
 
+# 比特流右移（向低位移动，看上去是左移）
+def right_shift(packed: torch.Tensor) -> torch.Tensor:
+    mask32 = torch.tensor((1 << 32) - 1, dtype=packed.dtype, device=packed.device)
+    unsigned = packed & mask32  # (..., N)
+    half = unsigned >> 1  # (..., N)
+    carry = (torch.roll(unsigned, shifts=1, dims=-1) & 1) << 31  # (..., N)
+    return (half | carry).to(packed.dtype)
+
+# 比特流依次右移n次并堆叠成新的张量，使用了for循环和上面的right_shift函数
+def right_shift_stack(packed, n):
+    '''
+    shift the input bit-stream rightwards by 0, 1, 2, ..., n bit circularly
+    and stack the these results as the penultimate dimension.
+    Note that "rightwards" here means toward the direction of higher bit positions(say, from 2^0 to 2^1)
+    :param packed: bit stream that needs to be shifted and stacked. length of dim -1 must be seq_len//32
+    :param n: times of shifts
+    :return: [... , n, seq_len//32]
+    '''
+    if n <= 0:
+        raise ValueError("n must be > 0")
+
+    outs = [packed]
+    cur = packed
+    for _k in range(1, n):
+        cur = right_shift(cur)
+        outs.append(cur)
+
+    return torch.stack(outs, dim=-2)
